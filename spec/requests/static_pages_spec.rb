42f1eae3428ad4b674796eb9a1810550e57e8b01
Tester <test>
2014-04-09 12:48:02 -0500

    stuff
    12

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+
+      before{visit  root_path}
+
+    it "should have the content Sample App" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+      page.should have_selector("h1", text: 'Third App')
+    end
+      it "should have a title" do
+      visit  root_path
+      page.should have_selector("title", text: 'Home Page')
+    end
+  end
+    describe "Help page" do
+    it "should have the content Help Page" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit  help_path
+      page.should have_selector("h1", text: 'Help Page')
+    end
+    it "should have a title" do
+      visit  help_path
+      page.should have_selector("title", text: 'Help Page')
+    end
+  end
+  describe "About Us page" do
+    it "should have the content About Us" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit  about_path
+      page.should have_selector("h1", text: 'About Us')
+    end
+    it "should have a title" do
+      visit  about_path
+      page.should have_selector("title", text: 'About Page')
+    end
+  describe "Contact page" do
+    it "should have the content Contact Us" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit  contact_path
+      page.should have_selector("h1", text: 'Contact Us')
+    end
+    it "should have a title" do
+      visit  contact_path
+      page.should have_selector("title", text: 'Contact Page')
+    end
+  end
+end
